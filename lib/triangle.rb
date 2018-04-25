e668d8c6ebba30fde2e667c8174e44bfed974e3f
Everett <everett.bowline@gmail.com>
2018-04-25 12:25:34 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,9 +25,10 @@ class TriangleError < StandardError
   end
 end
 
-#a <= 0 || b <= 0 || c <= 0 || (a + b) < c || (a + c) < b || (c + b) < a
+#if a <= 0 || b <= 0 || c <= 0 || (a + b) < c || (a + c) < b || (c + b) < a
 #      begin
 #	      raise TriangleError
 #      rescue TriangleError => error
 #	      puts error.message
 #      end
+#end
